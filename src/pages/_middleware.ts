ba59334d2c3e8e1872df639d08d6ffed90a71e6f
John Kennette Carrera Doce <jkcd@sitecore.net>
2022-04-22 15:10:51 -0400

    new nextjs app

--- src/pages/_middleware.ts
+++
@@ -1,7 +0,0 @@
-import type { NextRequest } from 'next/server';
-import middleware from 'lib/middleware';
-
-// eslint-disable-next-line
-export default async function (req: NextRequest) {
-  return middleware(req);
-}
